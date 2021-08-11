ed27c66e6799d78e67c21eb41aeb91dde1f81cf2
shavonthadani <shavon.thadani@gmail.com>
2021-08-10 20:48:19 -0400

    first commit

---
+++ jot-it-website /website/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+db = SQLAlchemy()
+DB_NAME = "database.db"
+def create_app():
+    #Create web app and database
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'aklsdfjal faskjdnfaskjdfn'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+
+    #import blueprints
+    from .views import views
+    from .auth import auth
+    #register blueprints
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    # import database models
+    from .models import User, Note
+    create_database(app)
+    #start off on login page
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def login_manager(id):
+        return User.query.get(int(id))
+
+
+    return app
+#create database
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print("Created database")
---
+++ jot-it-website /website/auth.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+auth = Blueprint('auth',__name__)
+from flask_login import login_user, login_required, logout_user, current_user
+
+#login page
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!',category = 'success')
+                login_user(user,remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incoreect password, try again.', category='error')
+        else:
+            flash("Email does not exist.",category='error')
+    return render_template("login.html", user=current_user)
+
+#logout
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+#sign up page
+@auth.route('/sign-up',methods=['GET','POST'])
+def sign_up():
+    if request.method == 'POST':
+        email =request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash("Email already exists",category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.',category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 characters.',category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.',category='error')
+        elif len(password1) < 7:
+            flash('Password must be longer than 6 characters.',category='error')
+            pass
+        else:
+            new_user = User(email=email,first_name=first_name,password=generate_password_hash(password1,method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user,remember=True)
+            flash('Account created.',category='success')
+            return redirect(url_for('views.home'))
+    return render_template("sign_up.html", user = current_user)
